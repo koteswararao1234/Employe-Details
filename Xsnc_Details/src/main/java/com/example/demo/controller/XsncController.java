fd7af2be1f06f6293226a12847cef186ab2ad0ab
hp <hp@DESKTOP-VKJ5V5A>
2019-12-25 20:29:06 +0530

    Added swagger

--- Xsnc_Details/src/main/java/com/example/demo/controller/XsncController.java
+++ Xsnc_Details/src/main/java/com/example/demo/controller/XsncController.java
@@ -1,5 +1,26 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.XsncDto;
+import com.example.demo.service.XsncService;
+
+@RestController
+@RequestMapping("xsnc")
 public class XsncController {
+
+	@Autowired
+	private XsncService sncService;
+
+	@PostMapping(value = "/create")
+	public void createXsncDetails(@RequestBody XsncDto sncDto) {
+
+		sncService.createXsncDetails(sncDto);
+
+	}
 
 }
--- Xsnc_Details/src/main/java/com/example/demo/service/XsncService.java
+++ Xsnc_Details/src/main/java/com/example/demo/service/XsncService.java
@@ -1,5 +1,25 @@
 package com.example.demo.service;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.dto.XsncDto;
+import com.example.demo.model.Xsnc;
+import com.example.demo.repository.XsncRepository;
+
+@Service
 public class XsncService {
+
+	@Autowired
+	private XsncRepository xsncRepository;
+
+	public void createXsncDetails(XsncDto sncDto) {
+
+		Xsnc xsnc = new Xsnc();
+		BeanUtils.copyProperties(sncDto, xsnc);
+		xsncRepository.save(xsnc);
+
+	}
 
 }
